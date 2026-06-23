1c2bc4df2bd53d65877c1ee74b98fb9d50212263
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-23 15:09:01 +0200

    Ajout d'un script Tailwind CSS

--- views/products/index.php
+++ views/products/index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Liste des produits</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 p-8">
 
-   
+    
 
 </body>
 </html>
