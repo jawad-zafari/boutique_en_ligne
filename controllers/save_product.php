ecf1f6c3a97b4862bc7de59ce80e5c31e838663c
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-23 15:16:57 +0200

    Ajout de la récupération des données du formulaire

--- controllers/save_product.php
+++ controllers/save_product.php
@@ -1,5 +1,13 @@
 <?php
 require_once '../config/database.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupérer les données du formulaire
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    $image_url = $_POST['image_url'];
 
+   
+}
 ?>
