8d11f4daf6d1a8343fd044a52c092124160c66e5
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-23 14:41:13 +0200

    Configuration pour récupérer les données sous forme de tableau associatif

--- config/database.php
+++ config/database.php
@@ -13,7 +13,11 @@ try {
     // Initialisation de la connexion PDO
     $pdo = new PDO($dsn, $user, $pass);
     
-   
+    // Configuration des erreurs pour le mode exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Configuration pour récupérer les données sous forme de tableau associatif
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
 } catch () {
     
