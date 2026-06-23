4bb38e17dd518ebe6256118df09962025f7d60ab
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-23 14:54:03 +0200

    Ajout de l'appel à la fonction getAllProducts pour récupérer tous les produits

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,5 +2,8 @@
 // Inclure le modèle (pour avoir accès à la fonction getAllProducts)
 require_once '../models/Product.php';
 
+// Appeler la fonction du modèle pour obtenir tous les produits
+$products = getAllProducts($pdo);
+
 
 ?>
