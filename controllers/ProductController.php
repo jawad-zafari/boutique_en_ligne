9d898552bd781f76c1fd970774f31421b01411b8
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-23 14:54:14 +0200

    Ajout de l'inclusion de la vue pour afficher les produits

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -5,5 +5,6 @@ require_once '../models/Product.php';
 // Appeler la fonction du modèle pour obtenir tous les produits
 $products = getAllProducts($pdo);
 
-
+// Inclure la vue pour afficher les produits
+require_once '../views/products/index.php';
 ?>
